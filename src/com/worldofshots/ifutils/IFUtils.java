b21e547dfcb0e47c1ece14f3aa6635c5f97b4809
Jesse Pavel <jpavel@alum.mit.edu>
2018-03-22 19:42:18 -0400

    Initial checkin

---
+++ src/com/worldofshots/ifutils/IFUtils.java
@@ -0,0 +1,68 @@
+package com.worldofshots.ifutils;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+import com.sanityinc.jargs.CmdLineParser;
+import com.sanityinc.jargs.CmdLineParser.Option;
+
+public class IFUtils
+{
+    private static String usageText;
+
+    public static void main(String[] args) throws IOException {
+        CmdLineParser parser = new CmdLineParser();
+        Option<Boolean> base64Opt = parser.addBooleanOption('6', "base64");
+        Option<Boolean> helpOpt = parser.addBooleanOption('h', "help");
+
+        try {
+            parser.parse(args);
+        } catch (CmdLineParser.OptionException e) {
+            println(e.getMessage() + "\n");
+            println(getUsageText());
+            System.exit(2);
+        }
+        String[] files = parser.getRemainingArgs();
+
+        // Commands
+        if (parser.getOptionValue(helpOpt, Boolean.FALSE)) {
+            println(getUsageText());
+            System.exit(0);
+        } else if (parser.getOptionValue(base64Opt, Boolean.FALSE)) {
+            if (files.length != 2) {
+                println(getUsageText());
+                System.exit(2);
+            }
+        }
+    }
+
+    private static String getUsageText() throws UnsupportedEncodingException {
+        if (usageText == null) {
+            InputStream in = IFUtils.class.getResourceAsStream("usage.txt");
+            Reader reader = new InputStreamReader(in, "UTF-8");
+            usageText = slurpReaderText(reader, 256);
+        }
+        return usageText;
+    }
+
+    static void println(String msg) {
+        System.out.println(msg);
+    }
+
+    static String slurpReaderText(Reader r, int bufferSize) {
+        String s = "";
+        try (BufferedReader br = new BufferedReader(r)) {
+            char [] buffer = new char[bufferSize];
+            StringBuilder sb = new StringBuilder(5*bufferSize);
+            int n;
+            while ((n = br.read(buffer, 0, buffer.length)) != -1)
+                sb.append(buffer, 0, n);
+            s = sb.toString();
+        } catch (IOException ex) {
+            // It's fine to do nothing and let the default empty string value of
+            // 's' be returned.
+        }
+        return s;
+    }
+}
